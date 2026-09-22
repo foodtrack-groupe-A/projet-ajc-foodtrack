e5f9bb745c9e7fefcdb5846a2ccb6c5a2cb76239
Nar-Eau <abdelghani.loudjani@gmail.com>
2026-09-22 15:19:48 +0200

    Module Compute Phase 1

---
+++ labs/projet-final/terraform/modules/compute/main.tf
@@ -0,0 +1,53 @@
+resource "google_container_cluster" "primary" {
+  name     = "foodtrack-${var.equipe}-cluster" # nom du cluster, convention par lettre d'equipe
+  location = var.zone                          # cluster zonal, pas regional (contrainte imposee)
+
+  remove_default_node_pool = true # on supprime le pool cree automatiquement par GKE
+  initial_node_count       = 1    # exige par l API meme si le pool est detruit juste apres
+
+  network    = var.network_id    # rattachement au VPC du module reseau
+  subnetwork = var.subnetwork_id # rattachement au sous-reseau du module reseau
+
+  ip_allocation_policy {                                   # active le mode VPC natif
+    cluster_secondary_range_name  = var.pods_range_name     # plage secondaire dediee aux pods
+    services_secondary_range_name = var.services_range_name # plage secondaire dediee aux services
+  }
+
+  private_cluster_config {                   # configuration des noeuds prives
+    enable_private_nodes    = true            # aucun noeud n'a d'IP publique
+    enable_private_endpoint = false           # le plan de controle reste joignable en public
+    master_ipv4_cidr_block  = var.master_cidr # plage IP interne reservee au plan de controle
+  }
+
+  # master_authorized_networks_config : EN ATTENTE (cf. discussion Lead livraison)
+
+  logging_service    = "logging.googleapis.com/kubernetes"    # active Cloud Logging sur les composants systeme
+  monitoring_service = "monitoring.googleapis.com/kubernetes" # active Cloud Monitoring sur les composants systeme
+
+  deletion_protection = false # false pendant le projet, a repasser a true avant la demo si besoin
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "foodtrack-${var.equipe}-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    preemptible  = false
+    machine_type = var.machine_type
+
+    # Compte de service dédié (Bonne pratique sécurité)
+    service_account = var.service_account_email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    labels = {
+      equipe      = var.equipe
+      environment = var.environment
+    }
+
+    tags = ["gke-node", "foodtrack-${var.equipe}"]
+  }
+}
---
+++ labs/projet-final/terraform/modules/compute/variables.tf
@@ -0,0 +1,59 @@
+variable "equipe" {
+  type        = string
+  description = "Lettre ou nom de l'équipe (ex: a, b, c)"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone GCP d'hébergement du cluster zonal"
+  default     = "europe-west9-a"
+}
+
+variable "network_id" {
+  type        = string
+  description = "ID ou nom du VPC"
+}
+
+variable "subnetwork_id" {
+  type        = string
+  description = "ID ou nom du sous-réseau"
+}
+
+variable "pods_range_name" {
+  type        = string
+  description = "Nom de la plage d'IP secondaire pour les Pods"
+}
+
+variable "services_range_name" {
+  type        = string
+  description = "Nom de la plage d'IP secondaire pour les Services"
+}
+
+variable "master_cidr" {
+  type        = string
+  description = "Plage CIDR /28 réservée au Master GKE"
+  default     = "172.16.0.0/28"
+}
+
+variable "node_count" {
+  type        = number
+  description = "Nombre de nœuds dans le node pool"
+  default     = 1
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Type de machine pour les nœuds"
+  default     = "e2-medium"
+}
+
+variable "service_account_email" {
+  type        = string
+  description = "Email du Service Account attaché aux nœuds"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environnement (dev, staging, prod)"
+  default     = "dev"
+}
